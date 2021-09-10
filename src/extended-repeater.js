3728c75b5c7af40e9fd026fa392f7cda489bb82d
Maksim <honestliar91@gmail.com>
2021-09-10 12:58:26 +0300

    done: repeater done

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, opt) {
+  let newStr = '';
+  if (opt.additionRepeatTimes>0){
+    str+=Array(opt.additionRepeatTimes).fill(String(opt.addition)).join(opt.additionSeparator || '|');
+  } else if (!opt.additionRepeatTimes && opt.addition) {str+=opt.addition;}
+  if (opt.repeatTimes>0) {
+    newStr+=Array(opt.repeatTimes).fill(String(str)).join(opt.separator || '+');
+  }
+  return newStr?newStr:str;
 }
