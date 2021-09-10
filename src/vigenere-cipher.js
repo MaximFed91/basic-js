fdb9654da2afbf58562ee7eca5bf1a714f811d23
Maksim <honestliar91@gmail.com>
2021-09-10 15:39:13 +0300

    done:vigenere cipher done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+  NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,12 +22,52 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(b) {
+    this.b = b;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) throw new Error('Incorrect arguments!');
+    const keyOk = key.repeat(Math.ceil(str.length / key.length)).toUpperCase(),
+      strOk = str.toUpperCase();
+    let result = '',
+      j=0;
+    for (let i =0; i<str.length; i++){
+      let shift = strOk.charCodeAt(i) + keyOk.charCodeAt(i-j)-65;
+      if (shift>90) shift-=26;
+      if(strOk.charCodeAt(i)>64 && strOk.charCodeAt(i)<91){
+        result+=String.fromCharCode(shift);
+      } else {
+        j++;
+        result+=strOk[i];
+      }
+    }
+    if(this.b === undefined || this.b){
+      return result.toUpperCase();
+    } else {
+      return result.split('').reverse().join('').toUpperCase();
+    }
+  }
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) throw new Error('Incorrect arguments!');
+    const keyOk = key.repeat(Math.ceil(str.length / key.length)).toUpperCase(),
+      strOk = str.toUpperCase();
+    let result = '',
+      j=0;
+    for (let i =0; i<str.length; i++){
+      let shift = strOk.charCodeAt(i) - (keyOk.charCodeAt(i-j)-65);
+      if (shift<65) shift+=26;
+      if(strOk.charCodeAt(i)>64 && strOk.charCodeAt(i)<91){
+        result+=String.fromCharCode(shift);
+      } else {
+        j++;
+        result+=strOk[i];
+      }
+    }
+    if(this.b === undefined || this.b){
+      return result.toUpperCase();
+    } else {
+      return result.split('').reverse().join('').toUpperCase();
+    }
+
   }
 }
